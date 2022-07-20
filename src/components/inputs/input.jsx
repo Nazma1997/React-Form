bfe189b61caa951792760abce3bb567f91e78a79
Nazma1997 <nazma.ist.707@gmail.com>
2022-07-20 15:54:28 +0600

    checkbox

--- src/components/inputs/input.jsx
+++ src/components/inputs/input.jsx
@@ -7,6 +7,8 @@ class Inputs extends Component {
         country: '',
         bio: '',
         birthDate: '',
+        gender: '',
+        agree: false
       }
 
       handleChange = event => {
@@ -16,12 +18,17 @@ class Inputs extends Component {
       }
 
 
+      handleCheckbox = event => {
+        this.setState({agree: event.target.checked})
+      }
+
+
       handleSubmit = ()=> {
         console.log(this.state)
       }
   render(){
 
-    const {name, country, bio, birthDate} = this.state
+    const {name, country, bio, birthDate, agree} = this.state
     return(
       <div className='container'>
            
@@ -29,19 +36,29 @@ class Inputs extends Component {
          <div>
          <input onChange={this.handleChange} className='form-control my-2' type="text" name='name' placeholder='Please Enter Your Name' value={name}/>
 
-        <select value={country} onChange={this.handleChange} name="country" className='form-control my-2' >
-          <option value="Select Country">Select Country</option>
-          <option value="Bangladesh">Bangladesh</option>
-          <option value="India">India</option>
-          <option value="Pakistan">Pakistan</option>
-          <option value="China">China</option>
-        </select>
+           <select value={country} onChange={this.handleChange} name="country" className='form-control my-2' >
+              <option value="Select Country">Select Country</option>
+              <option value="Bangladesh">Bangladesh</option>
+              <option value="India">India</option>
+              <option value="Pakistan">Pakistan</option>
+              <option value="China">China</option>
+           </select>
 
-        <textarea value={bio} onChange={this.handleChange} name="bio" placeholder='Tell me about yourself' className='form-control my-2'></textarea>
+            <textarea value={bio} onChange={this.handleChange} name="bio" placeholder='Tell me about yourself' className='form-control my-2'></textarea>
          
          <input value={birthDate} type="date" onChange={this.handleChange} className='form-control my-2' name='birthDate' />
          
 
+         </div>
+
+         <div>
+              <input type="radio" name='gender' value='Male' onChange={this.handleChange} />  Male
+              <input className='ms-2' type="radio" name='gender' value='Female' onChange={this.handleChange} />  Female
+              <input type="radio" className='ms-2' name='gender' value='Others' onChange={this.handleChange} />  Others
+         </div>
+
+         <div>
+          <input type="checkbox" name='agree' checked={agree} onChange={this.handleCheckbox}/> I agree this terms and policy
          </div>
          
          <button onClick={this.handleSubmit}>Show Data</button>
